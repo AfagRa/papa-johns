3fc351ae067c05e69706a6ec6b61889961a43c5a
AfagRa <afaq010407@gmail.com>
2025-06-28 11:18:45 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl text-red-500 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
